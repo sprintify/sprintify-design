e693f3a37f652c4d010aadc572cd889542b2b358
Vegar Dietz <post@digitalkopi.no>
2017-10-01 14:57:06 +0200

    Included footer and video

--- js/script.js
+++ js/script.js
@@ -136,3 +136,26 @@ $().ready (function () {
 	});
 
 });
+
+//Footer accordion
+$(document).ready(function() {
+
+    (function ($) {
+        $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+
+        $('.tab ul.tabs li a').click(function (g) {
+            var tab = $(this).closest('.tab'),
+                index = $(this).closest('li').index();
+
+            tab.find('ul.tabs > li').removeClass('current');
+            $(this).closest('li').addClass('current');
+
+            tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').hide();
+            tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').show();
+
+            g.preventDefault();
+
+        } );
+    })(jQuery);
+
+});

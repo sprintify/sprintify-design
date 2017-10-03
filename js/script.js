1aed65a5c34ee4b9242f33dba212537d47737645
Vegar Dietz <post@digitalkopi.no>
2017-10-03 17:49:28 +0200

    Gallery is finished

--- js/script.js
+++ js/script.js
@@ -137,6 +137,36 @@ $().ready (function () {
 
 });
 
+//Gallery codes for lightbox
+$(document).ready(function(){
+    $('header a').on('click', function(){
+        // Current class assigniment
+        $('header li.current').removeClass('current');
+        $(this).parent().addClass('current');
+
+        // Set heading text
+        $('h1#heading').text($(this).text());
+
+        // Get & filter link text
+        var category = $(this).text().toLowerCase().replace(' ','-');
+
+        // Remove hidden class if 'all-projects' is selected
+        if(category == 'all-projects') {
+            $('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+        } else {
+            $('ul#gallery li').each(function(){
+                if(!$(this).hasClass(category)){
+                    $(this).hide().addClass('hidden');
+                } else {
+                    $(this).fadeIn('slow').removeClass('hidden');
+                }
+            });
+        }
+        // Stop link behaviour
+        return false;
+    });
+});
+
 //Footer accordion
 $(document).ready(function() {
 
